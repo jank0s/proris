776fefee287ef753f6111970fc60ef99cdb4ea72
Jan <jan@k0s.si>
2013-03-30 03:57:25 +0100

    Added list_display and list_filter to Item admin

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from webapp.models import Balance, Group, BudgetUserGroup, BudgetUser, PoliticalBranch, Programme, Category, Item
 
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('budget_year', 'category', 'value')
+    list_filter = ['budget_year']
+
 admin.site.register(Balance)
 admin.site.register(Group)
 admin.site.register(BudgetUserGroup)
@@ -8,4 +12,4 @@ admin.site.register(BudgetUser)
 admin.site.register(PoliticalBranch)
 admin.site.register(Programme)
 admin.site.register(Category)
-admin.site.register(Item)
+admin.site.register(Item, ItemAdmin)
