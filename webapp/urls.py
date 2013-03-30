11afccffadef6cada6aa542aa71480f86f8707cc
Jan <jan@k0s.si>
2013-03-30 22:25:08 +0100

    Made some progress on new views

--- webapp/urls.py
+++ webapp/urls.py
@@ -5,4 +5,8 @@ from webapp import views
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
                        url(r'^pb/(?P<year>\d+)/$', views.pb, name='pb'),
+                       url(r'^pb/(?P<year>\d+)/(?P<pb_id>\d+)/$', views.pb_item, name='pb_item'),
+                       url(r'^bug/(?P<year>\d+)/$', views.bug, name='bug'),
+                       url(r'^bug/(?P<year>\d+)/(?P<bug_id>\d+)/$', views.bug_bu, name='bug_bu'),
+                       url(r'^bug/(?P<year>\d+)/(?P<bug_id>\d+)/(?P<bu_id>\d+)/$', views.bug_bu_item, name='bug_bu_item'),
 )
