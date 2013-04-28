2bcad43e7db817d3e03428d5fd7239ebee2561ff
Tilen <tilen.faganel@gmail.com>
2013-04-28 18:47:21 +0200

    Splited view.html into multiple templates

--- webapp/static/webapp/js/app.js
+++ webapp/static/webapp/js/app.js
@@ -1,8 +1,8 @@
 angular.module('proris', ['prorisServices']).
     config(['$routeProvider', function($routeProvider) {
         $routeProvider.
-            when('/pb/:year', {templateUrl: 'static/webapp/templates/view.html',   controller: PBCtrl}).
-            when('/bug/:year', {templateUrl: 'static/webapp/templates/view.html',   controller: BUGCtrl}).
+            when('/pb/:year', {templateUrl: 'static/webapp/templates/pb.html',   controller: PBCtrl}).
+            when('/bug/:year', {templateUrl: 'static/webapp/templates/bug.html',   controller: BUGCtrl}).
             when('/', {templateUrl: 'static/webapp/templates/index.html'}).
             otherwise({redirectTo: '/'});
     }]);
