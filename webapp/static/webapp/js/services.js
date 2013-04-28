4dd8213dd2a70d192a7329dd04d1b006e2393346
jank0s <jan@k0s.si>
2013-04-28 21:30:44 +0200

    ?forma=json moved to parametrs

--- webapp/static/webapp/js/services.js
+++ webapp/static/webapp/js/services.js
@@ -2,31 +2,31 @@ var factories = angular.module('prorisServices', ['ngResource']);
   
   
 factories.factory('PB', function($resource, $routeParams){
-        return $resource('api/v1/pb/?format=json', {}, {
-            query: {method:'GET', params:{year:$routeParams.year}, isArray:false}
+        return $resource('api/v1/pb', {}, {
+            query: {method:'GET', params:{format:"json", year:$routeParams.year}, isArray:false}
         });
     });
     
 factories.factory('BUG', function($resource, $routeParams){
-        return $resource('api/v1/bug/?format=json', {}, {
-            query: {method:'GET', params:{year:$routeParams.year}, isArray:false}
+        return $resource('api/v1/bug', {}, {
+            query: {method:'GET', params:{format:"json", year:$routeParams.year}, isArray:false}
         });
     });
 
 factories.factory('PBItem', function($resource, $routeParams){
-        return $resource('api/v1/category/?format=json', {}, {
-            query: {method:'GET', params:{year:$routeParams.year, pbid:$routeParams.pbid},isArray:false}
+        return $resource('api/v1/category', {}, {
+            query: {method:'GET', params:{format:"json", year:$routeParams.year, pbid:$routeParams.pbid},isArray:false}
         });
     });
     
 factories.factory('BUGBu', function($resource, $routeParams){
-        return $resource('api/v1/bu/?format=json', {}, {
-            query: {method:'GET', params:{year:$routeParams.year, bugid:$routeParams.bugid}, isArray:false}
+        return $resource('api/v1/bu', {}, {
+            query: {method:'GET', params:{format:"json", year:$routeParams.year, bugid:$routeParams.bugid}, isArray:false}
         });
     });
     
 factories.factory('BUGBuItem', function($resource, $routeParams){
-        return $resource('api/v1/category/?format=json', {}, {
-            query: {method:'GET', params:{year:$routeParams.year, bugid:$routeParams.bugid, buid:$routeParams.buid}, isArray:false}
+        return $resource('api/v1/category', {}, {
+            query: {method:'GET', params:{format:"json", year:$routeParams.year, bugid:$routeParams.bugid, buid:$routeParams.buid}, isArray:false}
         });
     });
