f5038accc7bc4598e57b858041943f1edce69685
Tilen <tilen.faganel@gmail.com>
2013-04-29 00:47:41 +0200

    Enabled pie charts for every page

--- webapp/static/webapp/js/controllers.js
+++ webapp/static/webapp/js/controllers.js
@@ -35,3 +35,59 @@ function PBPieCtrl($scope){
     
     $scope.data = data1;
 }
+
+function PBItemPieCtrl($scope){
+    
+    var data1 = [
+			{ label: "Series1",  data: 110},
+			{ label: "Series2",  data: 80},
+			{ label: "Series3",  data: 70},
+			{ label: "Series4",  data: 90},
+			{ label: "Series5",  data: 30},
+			{ label: "Series6",  data: 10}
+		];
+    
+    $scope.data = data1;
+}
+
+function BUGPieCtrl($scope){
+    
+    var data1 = [
+			{ label: "Series1",  data: 13},
+			{ label: "Series2",  data: 50},
+			{ label: "Series3",  data: 42},
+			{ label: "Series4",  data: 70},
+			{ label: "Series5",  data: 20},
+			{ label: "Series6",  data: 74}
+		];
+    
+    $scope.data = data1;
+}
+
+function BUGBuPieCtrl($scope){
+    
+    var data1 = [
+			{ label: "Series1",  data: 10},
+			{ label: "Series2",  data: 30},
+			{ label: "Series3",  data: 90},
+			{ label: "Series4",  data: 70},
+			{ label: "Series5",  data: 80},
+			{ label: "Series6",  data: 110}
+		];
+    
+    $scope.data = data1;
+}
+
+function BUGBuItemPieCtrl($scope){
+    
+    var data1 = [
+			{ label: "Series1",  data: 74},
+			{ label: "Series2",  data: 75},
+			{ label: "Series3",  data: 33},
+			{ label: "Series4",  data: 43},
+			{ label: "Series5",  data: 80},
+			{ label: "Series6",  data: 200}
+		];
+    
+    $scope.data = data1;
+}
