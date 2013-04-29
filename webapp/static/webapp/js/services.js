ca6e4d6306fa1882312a646f8a09d975338468bf
jank0s <jan@k0s.si>
2013-04-29 02:56:15 +0200

    Angular template update

--- webapp/static/webapp/js/services.js
+++ webapp/static/webapp/js/services.js
@@ -3,30 +3,30 @@ var factories = angular.module('prorisServices', ['ngResource']);
   
 factories.factory('PB', function($resource, $routeParams){
         return $resource('pb/:year', {}, {
-            query: {method:'GET', params:{year:$routeParams.year}, isArray:false}
+            query: {method:'GET', params:{year:$routeParams.year}, isArray:true}
         });
     });
     
 factories.factory('BUG', function($resource, $routeParams){
         return $resource('bug/:year', {}, {
-            query: {method:'GET', params:{year:$routeParams.year}, isArray:false}
+            query: {method:'GET', params:{year:$routeParams.year}, isArray:true}
         });
     });
 
 factories.factory('PBItem', function($resource, $routeParams){
         return $resource('pb/:year/:pbid', {}, {
-            query: {method:'GET', params:{year:$routeParams.year, pbid:$routeParams.pbid},isArray:false}
+            query: {method:'GET', params:{year:$routeParams.year, pbid:$routeParams.pbid}, isArray:true}
         });
     });
     
 factories.factory('BUGBu', function($resource, $routeParams){
         return $resource('bug/:year/:bugid', {}, {
-            query: {method:'GET', params:{year:$routeParams.year, bugid:$routeParams.bugid}, isArray:false}
+            query: {method:'GET', params:{year:$routeParams.year, bugid:$routeParams.bugid}, isArray:true}
         });
     });
     
 factories.factory('BUGBuItem', function($resource, $routeParams){
         return $resource('bug/:year/:bugid/:buid', {}, {
-            query: {method:'GET', params:{year:$routeParams.year, bugid:$routeParams.bugid, buid:$routeParams.buid}, isArray:false}
+            query: {method:'GET', params:{year:$routeParams.year, bugid:$routeParams.bugid, buid:$routeParams.buid}, isArray:true}
         });
     });
