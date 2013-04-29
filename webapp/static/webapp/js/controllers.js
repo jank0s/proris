888d4937b9b275c264105deb722b223096dcedeb
Tilen <tilen.faganel@gmail.com>
2013-04-29 05:12:53 +0200

    Added placeholders for graphs

--- webapp/static/webapp/js/controllers.js
+++ webapp/static/webapp/js/controllers.js
@@ -1,9 +1,19 @@
 function PBCtrl($scope, $routeParams, $http) {
-    
+   
     $http.get('pb/' + $routeParams.year).success(function(data) {
-        $scope.its = data;
+        $scope.its = data;    
     });
+   
+    var datapie = [];
     
+    for (var i = 0; i < 10; i++) {
+	    	var temp = {};
+	    	temp.label = "Placeholder";
+	    	temp.data = Math.random();
+	    	datapie.push(temp);
+	}
+    
+	$scope.pie = datapie;
     $scope.year = $routeParams.year;
 }
 
@@ -13,6 +23,17 @@ function BUGCtrl($scope, $routeParams, $http) {
         $scope.its = data;
     });
     
+    var datapie = [];
+    
+    for (var i = 0; i < 10; i++) {
+	    	var temp = {};
+	    	temp.label = "Placeholder";
+	    	temp.data = Math.random();
+	    	datapie.push(temp);
+	}
+    
+	$scope.pie = datapie;
+    
 	$scope.year = $routeParams.year;
 }
 
@@ -21,6 +42,17 @@ function PBItemCtrl($scope, $routeParams, $http) {
 	$http.get('pb/' + $routeParams.year + '/' + $routeParams.pbid).success(function(data) {
         $scope.its = data;
     });
+    
+    var datapie = [];
+    
+    for (var i = 0; i < 10; i++) {
+	    	var temp = {};
+	    	temp.label = "Placeholder";
+	    	temp.data = Math.random();
+	    	datapie.push(temp);
+	}
+    
+	$scope.pie = datapie;
 }
 
 function BUGBuCtrl($scope, $routeParams, $http) {
@@ -28,6 +60,17 @@ function BUGBuCtrl($scope, $routeParams, $http) {
 	$http.get('bug/' + $routeParams.year + '/' + $routeParams.bugid).success(function(data) {
         $scope.its = data;
     });
+    
+    var datapie = [];
+    
+    for (var i = 0; i < 10; i++) {
+	    	var temp = {};
+	    	temp.label = "Placeholder";
+	    	temp.data = Math.random();
+	    	datapie.push(temp);
+	}
+    
+	$scope.pie = datapie;
 	
 	$scope.year = $routeParams.year;
 	$scope.bugid = $routeParams.bugid;
@@ -38,74 +81,15 @@ function BUGBuItemCtrl($scope, $routeParams, $http) {
 	$http.get('bug/' + $routeParams.year + '/' + $routeParams.bugid + '/' + $routeParams.buid).success(function(data) {
         $scope.its = data;
     });
-}
-
-function PBPieCtrl($scope){
     
-    var data1 = [
-			{ label: "Series1",  data: 10},
-			{ label: "Series2",  data: 30},
-			{ label: "Series3",  data: 90},
-			{ label: "Series4",  data: 70},
-			{ label: "Series5",  data: 80},
-			{ label: "Series6",  data: 110}
-		];
+    var datapie = [];
     
-    $scope.data = data1;
-}
-
-function PBItemPieCtrl($scope){
+    for (var i = 0; i < 10; i++) {
+	    	var temp = {};
+	    	temp.label = "Placeholder";
+	    	temp.data = Math.random();
+	    	datapie.push(temp);
+	}
     
-    var data1 = [
-			{ label: "Series1",  data: 110},
-			{ label: "Series2",  data: 80},
-			{ label: "Series3",  data: 70},
-			{ label: "Series4",  data: 90},
-			{ label: "Series5",  data: 30},
-			{ label: "Series6",  data: 10}
-		];
-    
-    $scope.data = data1;
-}
-
-function BUGPieCtrl($scope){
-    
-    var data1 = [
-			{ label: "Series1",  data: 13},
-			{ label: "Series2",  data: 50},
-			{ label: "Series3",  data: 42},
-			{ label: "Series4",  data: 70},
-			{ label: "Series5",  data: 20},
-			{ label: "Series6",  data: 74}
-		];
-    
-    $scope.data = data1;
-}
-
-function BUGBuPieCtrl($scope){
-    
-    var data1 = [
-			{ label: "Series1",  data: 10},
-			{ label: "Series2",  data: 30},
-			{ label: "Series3",  data: 90},
-			{ label: "Series4",  data: 70},
-			{ label: "Series5",  data: 80},
-			{ label: "Series6",  data: 110}
-		];
-    
-    $scope.data = data1;
-}
-
-function BUGBuItemPieCtrl($scope){
-    
-    var data1 = [
-			{ label: "Series1",  data: 74},
-			{ label: "Series2",  data: 75},
-			{ label: "Series3",  data: 33},
-			{ label: "Series4",  data: 43},
-			{ label: "Series5",  data: 80},
-			{ label: "Series6",  data: 200}
-		];
-    
-    $scope.data = data1;
+	$scope.pie = datapie;
 }
