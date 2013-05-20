7dbda854195550433f2e4462d4b24f24329d54c5
edin5 <edinmahmutovic5@gmail.com>
2013-05-21 00:44:08 +0200

    Year selection update

--- webapp/static/webapp/js/custom.js
+++ webapp/static/webapp/js/custom.js
@@ -1,3 +1,11 @@
 function load(){ 
-	$("tbody > tr:odd").addClass("odd");	
+	$("tbody > tr:odd").addClass("odd");
+
+	var url = window.location.href;
+	$("#yearSelect option").each(function(){
+	    if(url.indexOf($(this).text()) !== -1)
+	    {
+			$(this).attr('selected', 'selected');
+		}
+	});
 };
