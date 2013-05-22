30edfdb7c5664b27afd47ce38fb60486250c2d72
Edin Mahmutovic <edinmahmutovic5@gmail.com>
2013-05-22 22:03:05 +0200

    Bug fixes

--- webapp/static/webapp/js/custom.js
+++ webapp/static/webapp/js/custom.js
@@ -1,3 +1,12 @@
 function load(){
-    $("tbody > tr:odd").addClass("odd");
+        $("tbody > tr:odd").addClass("odd");
+
+        var url = window.location.href;
+        $("#yearSelect option").each(function(){
+            $(this).attr('selected', false);
+            if(url.indexOf($(this).text()) !== -1)
+            {
+                $(this).prop('selected', true).change();
+            }
+        });
 };
