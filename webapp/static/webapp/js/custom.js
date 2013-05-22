8aa1eca4e68d6e5a47f802852528ac519c0a1992
Edin Mahmutovic <edinmahmutovic5@gmail.com>
2013-05-22 19:35:55 +0200

    Update

--- webapp/static/webapp/js/custom.js
+++ webapp/static/webapp/js/custom.js
@@ -1,16 +1,3 @@
 function load(){
     $("tbody > tr:odd").addClass("odd");
 };
-
-function load(){
-    $("tbody > tr:odd").addClass("odd");
-
-    var url = window.location.href;
-    $("#yearSelect option").each(function(){
-        $(this).attr('selected', false);
-        if(url.indexOf($(this).text()) !== -1)
-        {
-            $(this).prop('selected', true).change();
-        }
-    });
-};
