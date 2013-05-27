f58c990e6078822b4be1082cb2e3e91251b77dfd
Tilen Faganel <tilen.faganel@gmail.com>
2013-05-27 15:58:19 +0200

    Removed tastypie.

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,24 +1,8 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 
-from tastypie.api import Api
-from webapp.resources import BalanceResource, GroupResource
-from webapp.resources import BudgerUserGroupResource, BudgetUserResource
-from webapp.resources import PoliticalBranchResource, ProgrammeResource
-from webapp.resources import CategoryResource, ItemResource
 from webapp import views
 
-v1_api = Api(api_name='v1')
-v1_api.register(BalanceResource())
-v1_api.register(GroupResource())
-v1_api.register(BudgerUserGroupResource())
-v1_api.register(BudgetUserResource())
-v1_api.register(PoliticalBranchResource())
-v1_api.register(ProgrammeResource())
-v1_api.register(CategoryResource())
-v1_api.register(ItemResource())
-
 urlpatterns = patterns('',
-                       url(r'^api/', include(v1_api.urls)),
                        url(r'^$', views.index, name='index'),
 
                        url(r'^year/$', views.year, name='year'),
